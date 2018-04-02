5c4c8330250f9de72923b803251a645158741e8e
angelabauer <angelabauer@users.noreply.github.com>
2018-04-02 18:00:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+function bmiCalculator(weight, height) {
+    //TODO
+    var bmi = weight / Math.pow(height, 2);
+    return bmi;
+}
+
+
+describe('bmiCalculator()', function() {
+    
+    it('should work for the example given', function() {
+        expect(bmiCalculator(60, 2)).toEqual(15);
+    });
+});
+
+// load jasmine htmlReporter
+(function() {
+  var env = jasmine.getEnv();
+  env.addReporter(new jasmine.HtmlReporter());
+  env.execute();
+}());
